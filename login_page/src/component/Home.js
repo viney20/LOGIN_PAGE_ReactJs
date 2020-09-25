f965b51dd50c7573d6a32b149b39390e01b22cd0
viney20 <vineygautam.21@gmail.com>
2020-09-26 00:29:39 +0530

    create 2 component login and signup
     component:
    1:login
    2: signup

--- login_page/src/component/Home.js
+++ login_page/src/component/Home.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Button } from "@material-ui/core";
 import "./Home.css";
+import Login from "./Login";
+import Signup from "./Signup";
 function home() {
   return (
     <div className="home">
---
+++ login_page/src/component/Signup.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./Signup.css";
+function Signup() {
+  return <div></div>;
+}
+
+export default Signup;
